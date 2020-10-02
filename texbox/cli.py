099f78939c413e9231b82ef4635a5c2949fd20da
João Palmeiro <joaommpalmeiro@gmail.com>
2020-10-02 17:09:05 +0100

    Closes #3

--- texbox/cli.py
+++ texbox/cli.py
@@ -1,7 +1,16 @@
 from pathlib import Path
 
+from .constants import Acronym, Term
 from .parser import acronym_parser, acronym_writer
 from .utils import natural_sort_list_of_lists, split_irregular_list_of_lists
+
+
+def summary(acronyms, order="ascending"):
+    n_acronyms = len(acronyms)
+
+    print(
+        f"✨ {Term.BOLD}{n_acronyms}{Term.END} acronyms sorted in {Term.BOLD}{order}{Term.END} order!"
+    )
 
 
 def main():
@@ -13,13 +22,15 @@ def main():
     parsed_acronyms = parser.parseString(original_acronyms, parseAll=True).asList()
 
     comments, structured_acronyms = split_irregular_list_of_lists(parsed_acronyms)
-    structured_acronyms = natural_sort_list_of_lists(structured_acronyms, 0)
+    structured_acronyms = natural_sort_list_of_lists(
+        structured_acronyms, Acronym["LABEL"]
+    )
 
     output = acronym_writer(comments, structured_acronyms)
 
-    print(output)
+    path.write_text(output)
 
-    # path.write_text(output)
+    summary(structured_acronyms)
 
 
 if __name__ == "__main__":
--- texbox/constants.py
+++ texbox/constants.py
@@ -1,4 +1,17 @@
+from enum import IntEnum
+
 ACRONYM_MACRO = "\\newacronym"
 COMMENT_CHAR = "%"
 EMPTY_LINE = [""]
 SUPPORTED_PUNCTUATION = "-.+"
+
+
+class Term:
+    BOLD = "\033[1m"
+    END = "\033[0m"
+
+
+class Acronym(IntEnum):
+    LABEL = 0
+    ABBREV = 1
+    FULL = 2
